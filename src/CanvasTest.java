8db03160268d1d61156f074375f6054962a15728
keisuke123 <luckey.star123@gmail.com>
2013-06-28 17:30:48 +0900

    汚い
    汚い汚すぎる

--- src/CanvasTest.java
+++ src/CanvasTest.java
@@ -1,46 +1,37 @@
 import java.awt.*;
 import java.awt.event.*;
-
-class C extends Canvas{
- int s=600,x,y;
- Image I;
- Graphics G;
- C(){
-  x=y=300;
+class F extends Frame{
+ int s=600,x,y,t=300;
+ class W extends WindowAdapter{
+  public void windowClosing(WindowEvent e){System.exit(0);}
+ }
+ F(){
+  addWindowListener(new W());
+  add(new C());
   setSize(s,s);
-  addMouseMotionListener(new MouseMotionListener(){
+  setVisible(1>0);
+ }
+ class C extends Canvas{
+  C(){
+   x=y=t;
+   setSize(s,s);
+   addMouseMotionListener(new MouseMotionListener(){
    public void mouseDragged(MouseEvent e){
     x=e.getX();y=e.getY();
     repaint();
    }
    public void mouseMoved(MouseEvent e){
-    x=y=300;
+    x=y=t;
     repaint();
    }
   });
  }
- public void update(Graphics g){
-  paint(G=(I=createImage(s,s)).getGraphics());
-  g.drawImage(I,0,0,this);
- }
  public void paint(Graphics g){
-  g.drawLine(s/2,s/2,x,y);
+  g.clearRect(0,0,s,s);
+  g.drawLine(t,t,x,y);
  }
 }
-class CT extends Frame{
- class W extends WindowAdapter{
-  public void windowClosing(WindowEvent e){
-   System.exit(0);
-  }
- }
- CT(){
-  addWindowListener(new W());
-  add(new C());
-  setSize(600,600);
-  setVisible(1>0);
-  setResizable(1<0);
- }
  public static void main(String[] a){
-  new CT();
+  new F();
  }
 }
